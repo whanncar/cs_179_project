159d0fcfeaaa6ed8c35b5523b1fa730d40915871
Wade Hann-Caruthers <whanncar@gmail.com>
2016-05-17 10:43:10 -0700

    Wrote tests for utils functions

--- tests/utils_tests.c
+++ tests/utils_tests.c
@@ -2,10 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+int test1();
+int test2();
+int test3();
+int test4();
+
+
+
 int main() {
 
     printf("%d\n", test1());
     printf("%d\n", test2());
+    printf("%d\n", test3());
+    printf("%d\n", test4());
 
     return 0;
 }
@@ -58,13 +69,135 @@ int test2() {
 
 
 
-/*
-data_matrix *new_data_matrix(int num_rows, int num_columns);
-void calculate_matrix_times_vector(data_matrix *,
-                                   data_vector *,
-                                   data_vector *result);
-void filter_vector(data_vector *input,
-                   data_vector *output,
-                   float (*filter)(float));
+int test3() {
 
-*/
+    data_matrix *m;
+    data_vector *row;
+    float *row_data;
+    data_vector *input_vector;
+    float *input_vector_data;
+    data_vector *result;
+    float *result_data;
+    data_vector *expected_result;
+    float *expected_result_data;
+    int i;
+
+    /* Create and populate matrix */
+    m = new_data_matrix(3, 3);
+
+    row = (data_vector *) (m + 1);
+    row_data = (float *) (row + 1);
+
+    row_data[0] = 1;
+    row_data[1] = 7;
+    row_data[2] = 2;
+
+    row = (data_vector *) (((float *) (row + 1)) + row->size);
+    row_data = (float *) (row + 1);
+
+    row_data[0] = -2;
+    row_data[1] = 9;
+    row_data[2] = -3;
+
+    row = (data_vector *) (((float *) (row + 1)) + row->size);
+    row_data = (float *) (row + 1);
+
+    row_data[0] = 4;
+    row_data[1] = 5;
+    row_data[2] = 6;
+
+
+    /* Create and populate input vector */
+    input_vector = new_vector(3);
+
+    input_vector_data = (float *) (input_vector + 1);
+
+    input_vector_data[0] = 1;
+    input_vector_data[1] = -1;
+    input_vector_data[2] = 5;
+
+
+    /* Allocate space for result */
+    result = new_vector(3);
+
+    result_data = (float *) (result + 1);
+
+    /* Create and populate expected result */
+    expected_result = new_vector(3);
+
+    expected_result_data = (float *) (expected_result + 1);
+
+    expected_result_data[0] = 4;
+    expected_result_data[1] = -26;
+    expected_result_data[2] = 29;
+
+
+    /* Compute matrix times vector */
+    calculate_matrix_times_vector(m, input_vector, result);
+
+    /* Compare result to expected result */
+    for (i = 0; i < result->size; i++) {
+        if (result_data[i] != expected_result_data[i]) {
+            return 0;
+        }
+    }
+
+    return 1;
+
+}
+
+
+
+float square_filter(float x) {
+    return (x * x);
+}
+
+int test4() {
+
+    data_vector *input_vector;
+    float *input_vector_data;
+    data_vector *result;
+    float *result_data;
+    data_vector *expected_result;
+    float *expected_result_data;
+    int i;
+
+
+    /* Create and populate input vector */
+    input_vector = new_vector(3);
+
+    input_vector_data = (float *) (input_vector + 1);
+
+    input_vector_data[0] = 7;
+    input_vector_data[1] = 9;
+    input_vector_data[2] = -2;
+
+    /* Allocate space for result */
+    result = new_vector(3);
+
+    result_data = (float *) (result + 1);
+
+    /* Create and populate expected result */
+    expected_result = new_vector(3);
+
+    expected_result_data = (float *) (expected_result + 1);
+
+    expected_result_data[0] = 49;
+    expected_result_data[1] = 81;
+    expected_result_data[2] = 4;
+
+
+    /* Run the filter */
+    filter_vector(input_vector, result, &square_filter);
+
+
+    /* Compare result to expected result */
+    for (i = 0; i < result->size; i++) {
+        if (result_data[i] != expected_result_data[i]) {
+           return 0;
+        }
+    }
+
+    return 1;
+
+}
