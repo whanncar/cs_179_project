0ed8e833562902c26a8ff56d054ce59d3cfc3c7c
Wade Hann-Caruthers <whanncar@gmail.com>
2016-05-16 22:20:07 -0700

    Saving work

--- src/neural_net/neural_net_builder/neural_net_builder.c
+++ src/neural_net/neural_net_builder/neural_net_builder.c
@@ -9,6 +9,8 @@ neural_net *new_neural_net(neural_net_config *config) {
     layer_config_node *current_layer_config;
     data_vector **data_vector_ptrs;
 
+
+
     /* Allocate space for new neural net */
     new_net = (neural_net *) malloc(sizeof(neural_net));
 
@@ -17,19 +19,60 @@ neural_net *new_neural_net(neural_net_config *config) {
     new_net->layer_ptrs = (neural_layer **)
         malloc((new_net->num_layers) * sizeof(neural_layer *));
 
+    /* For each neural layer to be built */
     for (i = 0, current_layer_config = config->first;
          i < new_net->num_layers;
          i++, current_layer_config = current_layer_config->next) {
 
+        /* Allocate space for the new neural layer */
         (new_net->layer_ptrs)[i] = (neural_layer *)
                                       malloc(sizeof(neural_layer));
 
+        /* Allocate space for the new neural layer's weights */
         ((new_net->layer_ptrs)[i])->weights =
             new_data_matrix(current_layer_config->number_of_weights,
                             current_layer_config->input_vector_size);
-
     }
 
-    /* Finish writing me UNRESOLVED */
+
+
+    /* Allocate temporary array to hold data vector pointers */
+    data_vector_ptrs = (data_vector **) 
+          malloc((new_net->num_layers + 1) * sizeof(data_vector *));
+
+    /* For each neural layer */
+    for (i = 0, current_layer_config = config->first;
+         i < new_net->num_layers;
+         i++, current_layer_config = current_layer_config->next) {
+
+        /* Make new data vector of appropriate size */
+        data_vector_ptrs[i] = 
+            new_vector(current_layer_config->input_vector_size);
+    }
+
+    /* Make output data vector */
+    data_vector_ptrs[i] = new_vector(config->last->number_of_weights);
+
+
+
+    /* For each layer */
+    for (i = 0; i < new_net->num_layers; i++) {
+        /* Set input and output data pointers */
+        ((new_net->layer_ptrs)[i])->input_data = data_vector_ptrs[i];
+        ((new_net->layer_ptrs)[i])->output_data = data_vector_ptrs[i+1];
+    }
+
+
+
+    /* Set neural net input and output data pointers */
+    new_net->input_data = data_vector_ptrs[0];
+    new_net->output_data = data_vector_ptrs[i];
+
+
+
+    /* Clean up */
+    free(data_vector_ptrs);
+
+    return new_net;
 
 }
