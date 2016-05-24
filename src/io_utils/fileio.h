89ea15688d1ecf7882dc056ff7f7ba61c556656a
Wade Hann-Caruthers <whanncar@gmail.com>
2016-05-23 20:52:57 -0700

    Saving work

---
+++ src/io_utils/fileio.h
@@ -0,0 +1,10 @@
+#include "../neural_net/neural_net.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../utils/utils.h"
+
+
+
+sample_set *get_samples_from_file(char *filepath,
+                                  int num_samples,
+                                  int sample_length);
